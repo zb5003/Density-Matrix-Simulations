26eddcf4fc18824361857c1d4a2d25c7c01633ec
zb5003 <znb5003@gmail.com>
2017-08-07 11:13:38 -0500

    Update nlevel.py
    TESTING GITHUB TO PYCHARM

--- nlevel.py
+++ nlevel.py
@@ -6,7 +6,7 @@ from physicalconstants import *
 class NLevel_Evolve:
 
     version = 1
-
+FUCKFUCK
     def __init__(self, Dens_i, Gamma, Decay_to, open_system="no"):
         """
         Functions for calculating the time evolution of an atomic system.
