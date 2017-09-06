86be491299dd1f512eb07ee1f861f10aa0d9e99b
zb5003 <znb5003@gmail.com>
2017-09-06 14:35:43 -0500

    Reorganized class files into a package.  Also changed the timestep.

--- physicalconstants.py
+++
@@ -1,9 +0,0 @@
-# Various physical constants in SI units
-
-c = 3e8
-epsilon0 = 8.85e-12  # (s^4 A^2) / (m^3 kg)
-mu0 = 1.26e-6  # (m kg) / (s^2 A^2)
-hbar = 1.05e-34  # reduced Planck's constant
-muB = 9.274e-24  # Bohr Magneton (J/T)
-e_charge = 1.6e-19  # Electron charge
-a0 = 5.29e-11  # Bohr radius
